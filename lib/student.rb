56b071bc35c93ef4973feeff43d9b332bb106f8a
turpp <white.tripp@icloud.com>
2020-10-17 22:30:49 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
 
+
+    def initialize(knowledge=[])
+        @knowledge=knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
 end
